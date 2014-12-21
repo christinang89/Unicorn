5c1ca8e9a4689c9e191472a208ad1a382c804baa
Christina Ng <christinang89@gmail.com>
2014-12-20 20:40:37 -0800

    toggle function

--- Unicorn/ViewController.swift
+++ Unicorn/ViewController.swift
@@ -19,7 +19,69 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func toggle(sender: UISwitch) {
+       var s : String
+        if (sender.on) {
+            s = "1"
+        } else {
+            s = "0"
+        }
+        println(sender.on)
+        setLights("11", state: s)
+    }
+    
+    func checkLights() {
+        var url : String = "http://home.isidorechan.com/lights"
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+            
+            if (jsonResult != nil) {
+                // process jsonResult
+                println("test")
+                println(jsonResult["10"])
+            } else {
+                // couldn't load JSON, look at error
+                println(error)
+            }
+            
+        })
+    }
+    
+    func setLights(lightId : String, state : String) {
+        // create the request & response
+        var url : String = "http://home.isidorechan.com/lights/" + lightId
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        var response: NSURLResponse?
+        var error: NSError?
+        
+        // create some JSON data and configure the request
+        let jsonString = "{\"state\":\"" + state + "\"}"
+        request.URL = NSURL(string: url)
+        request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        request.HTTPMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // send the request
+        NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        
+        // look at the response
+        if let httpResponse = response as? NSHTTPURLResponse {
+            println("HTTP response: \(httpResponse.statusCode)")
+            println(response)
+        } else {
+            println("No HTTP response")
+            println(error)
+        }
+    }
 
+    
+    
 
 }
 
